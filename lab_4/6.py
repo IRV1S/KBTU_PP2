e7a1220eee9cc0172d59d3b3310e3907390e191a
IRV1S <erdaulet.aitzhanov.k@gmail.com>
2026-02-21 20:08:28 +0500

    lab_4

---
+++ lab_4/6.py
@@ -0,0 +1,12 @@
+def fibonacci_generator(n):
+    a, b = 0, 1
+    for _ in range(n):
+        yield a
+        a, b = b, a + b
+
+# Ввод данных
+n = int(input())
+
+# Генерация и вывод
+fib_numbers = list(fibonacci_generator(n))
+print(','.join(map(str, fib_numbers)))
